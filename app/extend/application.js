7f6cbc323b125bbc22952faf6f6ee475a9366ad7
rihel <569843207@qq.com>
2018-08-26 00:39:48 +0800

    Asdfdsaf

--- app/extend/application.js
+++ app/extend/application.js
@@ -2,12 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const nodemailer = require("nodemailer");
 const smtpTransport = require("nodemailer-smtp-transport");
+const nodemailer_wellknown_1 = require("nodemailer-wellknown");
 exports.default = {
     sendEmail(option) {
         return new Promise((resolve, reject) => {
-            let transporter = nodemailer.createTransport(smtpTransport(this.config.sendEmail));
+            const { auth, service } = this.config.sendEmail;
+            const config = nodemailer_wellknown_1.default(service);
+            config.auth = auth;
+            let transporter = nodemailer.createTransport(smtpTransport(config));
             const mailOptions = {
-                from: this.config.auth.user,
+                from: config.auth.user,
                 to: option.to,
                 subject: option.subject,
                 text: option.text,
